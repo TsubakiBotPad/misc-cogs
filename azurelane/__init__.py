3013af713999fa3ba5ae70f387febb8b43b33840
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-09-11 00:31:46 -0500

    remove all pad-related cogs

--- azurelane/__init__.py
+++
@@ -1,8 +0,0 @@
-from .azurelane import *
-
-__red_end_user_data_statement__ = "No personal data is stored."
-
-def setup(bot):
-    n = AzureLane(bot)
-    bot.add_cog(n)
-    bot.loop.create_task(n.reload_al())
--- damagecalc/__init__.py
+++
@@ -1,7 +0,0 @@
-from .damagecalc import *
-
-__red_end_user_data_statement__ = "No personal data is stored."
-
-def setup(bot):
-    n = DamageCalc(bot)
-    bot.add_cog(n)
