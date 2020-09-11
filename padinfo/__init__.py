d4811b1a734c9d0f27393a1b3ea5f45ab384812c
AA <chasehult@gmail.com>
2020-09-10 23:12:47 -0700

    remove padcogs

--- padinfo/__init__.py
+++
@@ -1,8 +0,0 @@
-from .padinfo import *
-
-__red_end_user_data_statement__ = "No personal data is stored."
-
-def setup(bot):
-    n = PadInfo(bot)
-    bot.add_cog(n)
-    bot.loop.create_task(n.reload_nicknames())
