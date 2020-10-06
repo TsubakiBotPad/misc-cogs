4cf14b009f1a15959d814a39cf57558ffdc964b6
AA <chasehult@gmail.com>
2020-10-06 13:44:12 -0700

    Override

--- selfroleoverride/__init__.py
+++ selfroleoverride/__init__.py
@@ -1,8 +1,12 @@
 from .selfroleoverride import *
+from redbot.core import errors
 
 __red_end_user_data_statement__ = "No personal data is stored."
 
 
 def setup(bot):
+    if bot.get_cog("Admin") is None:
+        raise errors.CogLoadError("Admin cog must be loaded to override selfrole.")
+        
     pdb = SelfRoleOverride(bot)
     bot.add_cog(pdb)
