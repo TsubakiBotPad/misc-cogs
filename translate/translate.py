ecf047c19fb961efbd7eadc539ecac2d07124f48
AA <chasehult@gmail.com>
2020-10-21 17:15:57 -0700

    Translate

--- translate/translate.py
+++ translate/translate.py
@@ -4,7 +4,6 @@ import json
 import logging
 import romkan
 import uuid
-from googleapiclient.discovery import build
 from redbot.core import Config, checks, commands
 from redbot.core.bot import Red
 from redbot.core.utils.chat_formatting import inline
@@ -19,9 +18,8 @@ class Translate(commands.Cog):
         super().__init__(*args, **kwargs)
         self.bot = bot
         self.config = Config.get_conf(self, identifier=724757473)
-        self.config.register_global(g_api_key=None, a_api_key=None)
+        self.config.register_global(a_api_key=None)
 
-        self.gservice = None
         self.aservice = None
 
     async def red_get_data_for_user(self, *, user_id):
@@ -42,17 +40,7 @@ class Translate(commands.Cog):
         """Translation utilities."""
 
     async def build_service(self):
-        api_key = await self.config.g_api_key()
-        if api_key:
-            try:
-                self.gservice = build('translate', 'v2', developerKey=api_key)
-                return
-            except:
-                logger.error("Google API invalid")
-
-        api_key = await self.config.a_api_key()
-        if api_key:
-            self.aservice = api_key
+        self.aservice = await self.config.a_api_key()
 
     @commands.command(aliases=['jaus', 'jpen', 'jpus'])
     @checks.bot_has_permissions(embed_links=True)
@@ -70,10 +58,6 @@ class Translate(commands.Cog):
     async def kanrom(self, ctx, *, query):
         """Transliterates Kanji to Romanji"""
         await ctx.send(romkan.to_roma(query))
-
-    async def g_translate_lang(self, source, target, query):
-        result = self.gservice.translations().list(source=source, target=target, format='text', q=query).execute()
-        return result.get('translations')[0].get('translatedText')
 
     async def a_translate_lang(self, source, target, query):
         endpoint = "https://api.cognitive.microsofttranslator.com"
@@ -104,28 +88,17 @@ class Translate(commands.Cog):
             await ctx.send(inline('Set up an API key first!'))
             return
 
-        if self.gservice:
-            translation = await self.g_translate_lang(source, target, query)
-        elif self.aservice:
+        if self.aservice:
             translation = await self.a_translate_lang(source, target, query)
         await ctx.send(embed=discord.Embed(description='**Original**\n`{}`\n\n**Translation**\n`{}`'.format(query, translation)))
 
     @translate.command()
-    async def setgkey(self, ctx, api_key):
-        """Sets the google api key."""
-        await self.config.g_api_key.set(api_key)
-        await ctx.tick()
-
-    @translate.command()
-    async def getgkey(self, ctx):
-        await ctx.author.send(await self.config.g_api_key())
-
-    @translate.command()
     async def setakey(self, ctx, api_key):
-        """Sets the google api key."""
+        """Sets the azure api key."""
         await self.config.a_api_key.set(api_key)
         await ctx.tick()
 
     @translate.command()
     async def getakey(self, ctx):
+        """Gets the azure api key."""
         await ctx.author.send(await self.config.a_api_key())
