519237a29c34c829ecb255af1081d84d427c7ff3
AA <chasehult@gmail.com>
2021-06-09 08:16:41 -0700

    hhhhh

--- onlineplot/onlineplot.py
+++ onlineplot/onlineplot.py
@@ -197,6 +197,7 @@ class OnlinePlot(commands.Cog):
 
         async with self.pool.acquire() as conn:
             async with conn.cursor() as cur:
+                print("args:", (guild.id, tzstr, weekday))
                 await cur.execute(GET_AVERAGES, (guild.id, tzstr, weekday))
                 rows = [[int(v) for v in row] for row in await cur.fetchall()]
 
