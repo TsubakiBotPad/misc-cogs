0a0f0eb752eaa414e16609ae0614d0127e0dd99d
Ethan <ethananhphan@gmail.com>
2022-10-17 18:15:17 -0400

    Add Twitter Listener

---
+++ twitterlistener/__init__.py
@@ -0,0 +1,8 @@
+from .twitterlistener import TwitterListener
+
+__red_end_user_data_statement__ = "No personal data is stored."
+
+
+async def setup(bot):
+    bot.add_cog(TwitterListener(bot)) if not __import__('asyncio').iscoroutinefunction(
+        bot.add_cog) else await bot.add_cog(TwitterListener(bot))
